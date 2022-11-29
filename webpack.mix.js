fbfa2f84a43296c789e6782ba7d9d02a1076db69
Sinmu <3038267725@qq.com>
2022-11-29 17:37:21 +0800

    删除了js文件

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,4 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css').version();
+mix.sass('resources/sass/app.scss', 'public/css').version();
